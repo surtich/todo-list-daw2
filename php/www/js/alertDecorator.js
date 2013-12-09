06a0357e60d405340b27a155dc3b4df187034ddb
IGZJavierPerez <surtich@gmail.com>
2013-12-09 12:57:47 +0100

    Using JQuery

--- php/www/js/alertDecorator.js
+++ php/www/js/alertDecorator.js
@@ -6,11 +6,11 @@
 
 	function alertDecorator(config, params, result) {
 		var alerts = config[0];
-		var alert = $('<div>').html(config[1]).addClass('alert ' + config[2]).fadeOut(4000, function() {
-      debugger
-      $(this).remove();
-    });
+		var alert = $('<div>').html(config[1]).addClass('alert ' + config[2]);
 		alerts.insertBefore(alert[0], alerts.children[0]);
+		alert.fadeOut(4000, function() {
+			$(this).remove();
+		});
 	}
 
 	window.addEventListener('load', function() {
@@ -22,14 +22,14 @@
 				[
 					'addTodo', 'delTodo', 'modTodo', 'checkTodo', 'checkAll', 'delChecked'
 				], {post: alertDecorator},
-				[
-					[alerts, 'Todo added', 'alert-info'],
-					[alerts, 'Todo deleted', 'alert-warning'],
-					[alerts, 'Todo updated', 'alert-info'],
-					[alerts, 'Todo checked/unchecked', 'alert-success'],
-					[alerts, 'All todos have changed their state', 'alert-success'],
-					[alerts, 'All todos have been deleted', 'alert-danger']
-				]);
+		[
+			[alerts, 'Todo added', 'alert-info'],
+			[alerts, 'Todo deleted', 'alert-warning'],
+			[alerts, 'Todo updated', 'alert-info'],
+			[alerts, 'Todo checked/unchecked', 'alert-success'],
+			[alerts, 'All todos have changed their state', 'alert-success'],
+			[alerts, 'All todos have been deleted', 'alert-danger']
+		]);
 	});
 
 })(this);
